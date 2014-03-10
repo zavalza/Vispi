cb4d3bb3097235d383db2d9985d802ea98d77dc9
Paul Zavalza <zse.paul@gmail.com>
2014-03-10 17:08:15 -0600

    Copy of the Little Duck homework
    These files will be useful as a base of the language

---
+++ PLY/patito_lex.py
@@ -0,0 +1,108 @@
+# ------------------------------------------------------------
+# patito.py
+#
+# scanner and parser for a simple language
+# ------------------------------------------------------------
+import ply.lex as lex
+
+reserved = {
+   'PROGRAM' : 'PROGRAM',
+   'VAR' : 'VAR',
+   'IF' : 'IF',
+   'ELSE' : 'ELSE',
+   'PRINT' : 'PRINT',
+   'int' : 'INT',
+   'float' : 'FLOAT',
+}
+# List of token names.
+tokens = [
+    'ID',
+    'CONSTANTE_I',
+    'CONSTANTE_F',
+    'CONSTANTE_STRING',
+    'PUNTO_Y_COMA',
+    'DOS_PUNTOS',
+    'COMA',
+    'IGUAL',
+    'MAS',
+    'MENOS',
+    'ASTERISCO',
+    'DIAGONAL',
+    'MAYOR_QUE',
+    'MENOR_QUE',
+    'MAYOR_MENOR',
+    'PARENTESIS_IZQ',
+    'PARENTESIS_DER',
+    'LLAVE_IZQ',
+    'LLAVE_DER',
+] + list(reserved.values())
+
+def t_ID(t):
+    r'[a-zA-Z][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+def t_CONSTANTE_F(t):
+    r'(\d+)(\.)(\d+)'
+    t.value = float(t.value)
+    return t
+
+def t_CONSTANTE_I(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_CONSTANTE_STRING = r'\"[^\n"]+\"'
+
+#literals = [+','-','*','/']
+t_PUNTO_Y_COMA = r';'
+t_DOS_PUNTOS = r':'
+t_COMA = r','
+t_IGUAL = r'='
+t_MAS = r'\+'
+t_MENOS = r'-'
+t_ASTERISCO = r'\*'
+t_DIAGONAL = r'\/'
+t_MAYOR_QUE = r'>'
+t_MENOR_QUE = r'<'
+t_MAYOR_MENOR = r'<>'
+t_PARENTESIS_IZQ = r'\('
+t_PARENTESIS_DER = r'\)'
+t_LLAVE_IZQ = r'\{'
+t_LLAVE_DER = r'\}'
+
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex(debug=0)
+
+# # Build the lexer
+# lexer = lex.lex()
+
+# ### Test the parser####
+# data = '''
+# PROGRAM test ; int
+# IF ( x > 4.0) { x = x + 1; }  ELSE {  x = x - 1;};}
+# '''
+
+# # Give the lexer some input
+# lexer.input(data)
+
+# # Tokenize
+# while True:
+#     tok = lexer.token()
+#     if not tok: break      # No more input
+#     print tok
+
---
+++ PLY/patito_parse.py
@@ -0,0 +1,123 @@
+from ply import *
+import patito_lex
+
+tokens = patito_lex.tokens
+
+
+#Grammatic rules
+def p_program(p):
+    'programa : declara_programa codigo'
+    print "Listo, programa exitoso!"
+
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_declara_programa(p):
+    'declara_programa : PROGRAM ID PUNTO_Y_COMA'
+
+def p_codigo(p):
+    '''codigo : bloque
+              | variables bloque'''
+
+def p_variables(p):
+    'variables : VAR declaracion'
+
+def p_declaracion(p):
+    '''declaracion : ID mas_ids DOS_PUNTOS tipo PUNTO_Y_COMA declaracion
+                    | empty '''
+
+def p_mas_ids(p):
+    '''mas_ids : empty
+                | COMA ID mas_ids'''
+
+def p_tipo(p):
+    '''tipo : INT
+            | FLOAT'''
+
+def p_bloque(p):
+    'bloque : LLAVE_IZQ estatuto LLAVE_DER'
+
+def p_estatuto(p):
+    '''estatuto : empty
+                | asignacion estatuto
+                | condicion estatuto
+                | escritura estatuto'''
+
+def p_asignacion(p):
+    'asignacion : ID IGUAL expresion PUNTO_Y_COMA'
+
+def p_escritura(p):
+    'escritura : PRINT PARENTESIS_IZQ evalua_e_imprime PARENTESIS_DER PUNTO_Y_COMA'
+
+def p_evalua_e_imprime(p):
+    '''evalua_e_imprime : expresion continua_imprime
+                        | CONSTANTE_STRING continua_imprime'''
+def p_continua_imprime(p):
+    '''continua_imprime :  empty
+                        | COMA evalua_e_imprime'''
+def p_condicion(p):
+    'condicion : IF PARENTESIS_IZQ expresion PARENTESIS_DER bloque otra_condicion PUNTO_Y_COMA'
+
+def p_otra_condicion(p):
+    '''otra_condicion : empty
+                        | ELSE bloque'''
+
+def p_expresion(p):
+    'expresion : exp mas_exp'
+
+def p_exp(p):
+    'exp :  termino mas_terminos'
+
+def p_mas_exp(p):
+    ''' mas_exp :  empty
+          | MAYOR_QUE exp
+          | MENOR_QUE exp
+          | MAYOR_MENOR exp '''
+
+def p_mas_terminos(p):
+    '''mas_terminos : empty
+                      | MAS exp
+                      | MENOS exp'''
+
+def p_termino(p):
+    'termino : factor mas_factores'
+
+def p_mas_factores(p):
+    '''mas_factores : empty
+                    | ASTERISCO termino
+                    | DIAGONAL termino'''
+
+def p_factor(p):
+    '''factor : PARENTESIS_IZQ expresion PARENTESIS_DER
+              | MAS var_cte
+              | MENOS var_cte
+              | var_cte'''
+
+def p_var_cte(p):
+    '''var_cte : ID
+                | CONSTANTE_I
+                | CONSTANTE_F'''
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+
+parser = yacc.yacc()
+
+def parse(data,debug=0):
+    parser.error = 0
+    p = parser.parse(data,debug=debug)
+    if parser.error: return None
+    return p
+
+# while 1:
+#     try:
+#         s = raw_input('calc > ')
+#     except EOFError:
+#         break
+#     if not s: continue
+#     yacc.parse(s)
